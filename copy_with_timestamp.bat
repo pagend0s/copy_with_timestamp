ce68a6c5d5aa7f66c5f11d5a76450f6da7167247
pagend0s <65870887+pagend0s@users.noreply.github.com>
2025-12-05 10:53:42 +0100

    Update copy_with_timestamp.bat

--- copy_with_timestamp.bat
+++ copy_with_timestamp.bat
@@ -6,6 +6,6 @@ GOTO PowerShell
 pushd %~dp0
 echo.
 CLS
-powershell -ExecutionPolicy Bypass -File .\Resources\copy_with_date_stamp.ps1
+powershell -ExecutionPolicy Bypass -File .\Resources\copy_with_date_stamp.ps1 -UseGui
 
 pause
